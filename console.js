9b7a716b9c04a4ab7b128afcaa18fbbc0462e17e
TheCR <thecreativecontrollab@gmail.com>
2019-04-29 08:13:53 -0500

    version 2.0

---
+++ console.js
@@ -0,0 +1,20 @@
+function
+	//   var userAnswer = prompt("Rock, Paper, Scissors");
+	// }
+	// var computerResponse = ["paper", "rock", "scissors"];
+	// var randomNum = Math.floor(Math.random() * 3);
+	// var computerAnswer = computerResponse[randomNum];
+	// console.log(
+	//   `Your choice is ${userAnswer}, the computer's choice is ${computerAnswer}.`
+	// );
+	// assign winner
+	// var Rock = 1;
+	// var Paper = 2;
+	// var Scissors = 3;
+	// document.getElementByID("button").addEventListener("click", newGame);
+	// document.getElementByID("bg").onclick(startGame);{
+	// 					function startGame() {
+	// 					console.log("New Game");
+	// }
+	// 	}
+	console() { }
